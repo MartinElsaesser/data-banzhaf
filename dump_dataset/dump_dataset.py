563f23580014a16cc028a02051a0dddd05c11391
Martin <dev-s1s2O6L@proton.me>
2026-05-29 10:52:12 +0000

    dump multiple datasets

--- dump_dataset/dump_dataset.py
+++ dump_dataset/dump_dataset.py
@@ -1,4 +1,4 @@
-dataset_name = "cpu"
+OpenML_dataset = ['fraud', 'apsfail', 'click', 'phoneme', 'wind', 'pol', 'creditcard', 'cpu', 'vehicle', '2dplanes']
 
 # extend path (https://stackoverflow.com/a/35273613)
 import os
@@ -9,18 +9,22 @@ if banzhaf_dir not in sys.path:
     print(f"add banzhaf dir to path: {banzhaf_dir}")
     sys.path.append(banzhaf_dir)
 
-# change cwd for dataset file path resolution
-print(f"switch cwd to banzhaf dir: {banzhaf_dir}")
-current_cwd = os.getcwd()
-os.chdir(banzhaf_dir)
-from prepare_data import get_data
-X, y, _, _ = get_data(dataset_name)
+def export_dataset(dataset_name):
+    current_cwd = os.getcwd()
+    os.chdir(banzhaf_dir) # change cwd for dataset file path resolution
+    from prepare_data import get_data
+    X, y, _, _ = get_data(dataset_name)
 
+    # export dataset as csv
+    import numpy as np
+    import pandas as pd
+    os.chdir(current_file_dir)  # change cwd for dumping csv into current folder
+    df = pd.DataFrame(np.concatenate((X, y.reshape((y.shape[0], 1))), axis=1))
+    export_dir="exports"
+    os.makedirs(export_dir, exist_ok=True)
+    df.to_csv(f"{export_dir}/{dataset_name}.csv")
+    os.chdir(current_cwd) # restore cwd
 
-# export dataset as csv
-import numpy as np
-import pandas as pd
-os.chdir(current_file_dir)  # used to dump csv into current folder
-df = pd.DataFrame(np.concatenate((X, y.reshape((y.shape[0], 1))), axis=1))
-df.to_csv(f"{dataset_name}.csv")
+for dataset_name in OpenML_dataset:
+    export_dataset(dataset_name)
 
