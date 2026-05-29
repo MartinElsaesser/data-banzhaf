0635499495c73dde2883eb2e57104b9042d0033e
Martin <dev-s1s2O6L@proton.me>
2026-05-29 10:41:59 +0000

    export dataset as csv

--- dump_dataset/dump_dataset.py
+++ dump_dataset/dump_dataset.py
@@ -1,20 +1,26 @@
-# add root dir as import path: https://stackoverflow.com/a/35273613
+dataset_name = "cpu"
+
+# extend path (https://stackoverflow.com/a/35273613)
 import os
 import sys
+current_file_dir = os.path.dirname(os.path.abspath(__file__))
+banzhaf_dir = os.path.normpath(os.path.join(current_file_dir, "../banzhaf"))
+if banzhaf_dir not in sys.path:
+    print(f"add banzhaf dir to path: {banzhaf_dir}")
+    sys.path.append(banzhaf_dir)
 
-module_path = "/workspaces/data-banzhaf-python/banzhaf"
-print(f"using module path: {module_path}")
-os.chdir(module_path)   # needed for correct dataset file path resolution
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
+# change cwd for dataset file path resolution
+print(f"switch cwd to banzhaf dir: {banzhaf_dir}")
+current_cwd = os.getcwd()
+os.chdir(banzhaf_dir)
 from prepare_data import get_data
+X, y, _, _ = get_data(dataset_name)
+
+
+# export dataset as csv
 import numpy as np
 import pandas as pd
-
-X, y, _, _ = get_data("cpu")
-
+os.chdir(current_file_dir)  # used to dump csv into current folder
 df = pd.DataFrame(np.concatenate((X, y.reshape((y.shape[0], 1))), axis=1))
+df.to_csv(f"{dataset_name}.csv")
 
-df.to_csv("cpu.csv")
-print(0)
